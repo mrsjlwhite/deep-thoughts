e4cee90047f1cd1f8a0c46bd6ac442d8c5a9323e
Jeslyn <jlwhite.codes@gmail.com>
2021-09-27 16:37:12 -0700

    💄Updated homepage, initial showing of Thoughts

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql'
+});
 
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
